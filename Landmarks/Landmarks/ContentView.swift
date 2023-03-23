4db88298351714eb22eff63aed14993df55e11ab
김규철 <rbcjf122@gmail.com>
2023-03-23 20:59:11 +0900

    Building Lists and Navigation

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,39 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, kyuchul")
-                    .font(.title)
-                HStack {
-                    Text("kyuchulkim")
-                    Spacer()
-                    Text("hothot")
-                }
-                .font(.subheadline)
-                .foregroundColor(.green)
-                
-                Divider()
-                
-                Text("kyuchulkimkyuchulkim")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,36 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by 김규철 on 2023/03/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable { //Identifiable Protocol
+    var id: Int // id가 존재하므로 Identifiable만 추가하면 Identifiable Protocol이 성립
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
